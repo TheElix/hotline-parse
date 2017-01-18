133e13c902ea785cdef24638d31c4ae4450fe542
Dmytro Huz <ualxdima@gmail.com>
2017-01-19 01:30:08 +0200

    Create connection to db

--- Connection.php
+++ Connection.php
@@ -2,5 +2,19 @@
 
 class Connection
 {
+    public static $user = 'root';
+    public static $password = 'root';
+    public static $dbName = 'hotline_items';
+    public static $dbPath = 'localhost';
+
+    public  static function make()
+    {
+        try{
+            return new PDO('mysql:host='.self::$dbPath.';dbname='.self::$dbName, self::$user, self::$password);
+        }
+        catch (PDOException $e){
+            die("Error with Connection\n");
+        }
+    }
 
 }
--- Query.php
+++ Query.php
@@ -3,4 +3,19 @@
 class Query
 {
 
+    protected $connection;
+
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function selectAll($table)
+    {
+        $statement = $this->connection->prepare("SELECT * FROM {$table}");
+
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_OBJ);
+    }
 }
