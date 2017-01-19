bdd1beca4178ded3f18a00255a8a6022201b0c1f
Dmytro Huz <ualxdima@gmail.com>
2017-01-20 01:35:25 +0200

    add saved method to DB

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 
 require 'Connection.php';
 require 'Query.php';
+require 'Request.php';
 require 'Page.php';
 require 'Item.php';
+
+$request = new Request('http://hotline.ua/computer/noutbuki-netbuki/872-883-9886-85763-85764-85765/?sort=0');
 
 $query = new Query(
     Connection::make()
 );
 
-$notebooks = $query->selectAll('notebook');
-
-print_r($notebooks);
+$notebooks = $query->addNotebookElements($request->getNotebooks());
