ee496d6556f29ae04b15930831a688d243f22c9a
Dmytro Huz <ualxdima@gmail.com>
2017-01-20 02:02:15 +0200

    delete empty classes

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 require 'Connection.php';
 require 'Query.php';
 require 'Request.php';
-require 'Page.php';
-require 'Item.php';
 
 $request = new Request('http://hotline.ua/computer/noutbuki-netbuki/872-883-9886-85763-85764-85765/?sort=0');
 
